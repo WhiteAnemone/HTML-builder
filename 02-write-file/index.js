5b4c996095098871320a11a8fd39e46112f4c303
white_anemone <Melissa-969@yandex.ru>
2023-05-09 15:39:39 +0300

    feat: solve task №2

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('node:readline');
+const { stdin: input, stdout: output, stderr: error } = require('node:process');
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+const rl = readline.createInterface({ input, output });
+
+rl.on('line', (input) => {
+  writeStream.write(input);
+  writeStream.write('\n');
+  if (input.includes('exit')) {
+    rl.close();
+    process.exit();
+  }
+});
+
+process.on('exit', code => {
+  if (code === 0) {
+    output.write('The text is written in file. Goobye!');
+  } else {
+    error.write(`Error: ${code}`);
+  }
+});
+
+// node 02-write-file
