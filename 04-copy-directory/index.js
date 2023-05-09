892e3300a0b70e2c7eb149a700b75f3ffb6cba46
white_anemone <Melissa-969@yandex.ru>
2023-05-10 01:22:51 +0300

    feat: solve task №4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err)
+      console.log(err);
+  });
+  fs.readdir(path.join(__dirname, 'files-copy'),
+    { withFileTypes: true },
+    (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) throw err;
+          });
+        })
+      }
+    })
+  fs.readdir(path.join(__dirname, 'files'),
+    { withFileTypes: true },
+    (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) {
+              console.log("Error Found:", err);
+            }
+          })
+        })
+      }
+    })
+  console.log('All files were successfully copied.');
+}
+
+copyDir();
+
+// node 04-copy-directory
